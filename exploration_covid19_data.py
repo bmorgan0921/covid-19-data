1d5d47f1773d90648813b8d06626c274c2d24ad1
B Morgan - Desktop <jammin0921@comcast.net>
2020-03-29 23:41:57 -0500

    initial exploring

--- exploration_covid19_data.py
+++ exploration_covid19_data.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('us-states.csv', index_col=0)
 
+states = list(set(data['state'].values))
+print(states)
+state_dict = {}
+for state in states:
+    state_dict[state] = data[data['state'] == state]
+    state_dict[state]['velocity_cases'] = state_dict[state]['cases'].diff()
+    state_dict[state]['acceleration_cases'] = state_dict[state]['velocity_cases'].diff()
+
+print(state_dict['Alabama'])
